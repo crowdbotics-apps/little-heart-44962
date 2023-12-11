f9f1a00b85e2dea0c9206d086d60d31a3d17066b
Crowdbotics <team@crowdbotics.com>
2023-12-11 18:42:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "little-heart-44962",
+  url: "https://little-heart-44962.botics.co",
+  api: "https://little-heart-44962.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
